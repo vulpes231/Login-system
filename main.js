02883dd976a586091e28d819220029a03484b425
Vulpes231 <whykayshot@gmail.com>
2023-06-17 12:51:25 -0400

    Added formfield validation to the form

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const passwordToggleBtn = document.querySelector(".password_toggle");
 const passwordInput = document.getElementById("password");
+const usernameInput = document.getElementById("username");
+
+// append button element
 passwordToggleBtn.innerHTML = 
 `
     <span class="material-symbols-outlined">
@@ -8,7 +11,7 @@ passwordToggleBtn.innerHTML =
 `;
 
 
-
+// visibility toggler function
 passwordToggleBtn.addEventListener("click", function() {
     if (passwordInput.type === "password") {
         passwordInput.type = "text";
@@ -24,3 +27,30 @@ passwordToggleBtn.addEventListener("click", function() {
             </span>`;
     }
 });
+
+
+// function to validate
+
+// get the username input 
+// is the usernam
+function handleSubmit() {
+    const userError = document.querySelector(".user_error")
+    const passError = document.querySelector(".pass_error")
+    const username = usernameInput.value
+
+    if (username.trim() === "") {
+        usernameInput.style.border = "1px solid red"
+        userError.textContent = "username must be at least 4 or more characters"
+    } else {
+        usernameInput.style.border = "1px solid darkgrey"
+        userError.textContent = ""
+        passError.textContent = ""
+    }
+    
+    if (passwordInput.value.trim() === "") {
+        passwordInput.style.border = "1px solid red"
+        passError.textContent = "Enter password"
+    } else {
+        passwordInput.style.border = "1px solid darkgrey"
+    }
+}
